077f636a277b4721b41d48bc86634df9cdbe0380
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-06-28 14:07:00 -0700

    clicking on pokemon gives its info

---
+++ main.js
@@ -0,0 +1,30 @@
+for (var i = 1; i <= 151; i++) {
+    var imgHTML = "<a href='#top-of-page'>";
+    imgHTML += "<img src='http://pokeapi.co/media/img/" + i + ".png'";
+    imgHTML += " id='" + i + "'>";
+    imgHTML += "</a>";
+    $('.pokedex').append(imgHTML);
+}
+
+$("img").click(function() {
+    var imgID = $(this).attr('id');
+    var url = "http://pokeapi.co/api/v1/pokemon/" + imgID;
+    $.get(url, function(res) {
+        var infoHTML = "<h2>" + res.name + "</h2>";
+        infoHTML += "<img src='http://pokeapi.co/media/img/" + imgID + ".png'>";
+
+        infoHTML += "<h4>Types</h4><ul>";
+        for (var i = 0; i < res.types.length; i++) {
+            infoHTML += "<li>" + res.types[i].name + "</li>";
+        }
+        infoHTML += "</ul>"
+
+        infoHTML += "<h4>Height</h4>";
+        infoHTML += res.height;
+
+        infoHTML += "<h4>Weight</h4>";
+        infoHTML += res.weight;
+
+        $(".pokemon-info").html(infoHTML);
+    }, 'json');
+});
